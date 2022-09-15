00961d28d089d7c8e082690ed599b5309c9dd60f
Sidiraos <raossidi@gmail.com>
2022-09-15 15:34:50 +0300

    V2 global converter programm can accept any value for convert

--- script.py
+++ script.py
@@ -1,64 +1,57 @@
-
-
-def demander_type_conversion_User() :
-    number_str = input ("1-pouces --> cm\n2-cm --> pouces\n--> ")
+CONSTANTE = 4212.48 #unite1 * constante = unite2
+unite1 = "dollars" #1 (unite1) * combien = unite2
+unite2 = "ariary"
+def demander_type_conversion(unite1 : str , unite2 : str) :
+    type_conversion_str = input (f"1-{unite1} --> {unite2}\n2-{unite2} --> {unite1}\n--> ")
     try :
-        number_int = int(number_str)
+        type_conversion_int = int(type_conversion_str)
     except:
         print('ERREUR : Vous devez rentrer un nombre pour continuer')
-        return demander_type_conversion_User()
+        return demander_type_conversion(unite1 , unite2 )
     else:
-        if number_int < 1 or number_int > 2 :
+        if type_conversion_int < 1 or type_conversion_int > 2 :
             print('ERREUR : Vous devez rentrer un nombre entre 1 et 2 pour continuer')
-            return demander_type_conversion_User()
-        elif number_int == 1 :
-            print('pouces --> cm')
-            return number_int
+            return demander_type_conversion(unite1 , unite2)
+        elif type_conversion_int == 1 :
+            print(f'CONVERSION {unite1.upper()} --> {unite2.upper()}')
         else :
-            print('cm --> pouces')
-            return number_int
+            print(f'CONVERSION {unite2.upper()} --> {unite1.upper()}')
 
-def obtenir_nombre_convertir(number_int):
-    if number_int == 1 :
-        nombre_convert_str = input('entrez le nombre de pouces\n--> ')
-        return nombre_convert_str
+    return type_conversion_int
 
-    elif number_int == 2 :
-        nombre_convert_str = input('entrez le nombre de cm\n--> ')
-        return nombre_convert_str
-
-def conversion (number_for_convert_str , number_int) :
+def obtenir_nombre_a_convertir(type_conversion : int, unite1 :str , unite2:str):
+    if type_conversion == 1 :
+        nombre_a_convertir_str = input(f'entrez le nombre de {unite1}\n--> ') 
+    else :
+        nombre_a_convertir_str = input(f'entrez le nombre de {unite2}\n--> ')
     try :
-       number_for_convert = float(number_for_convert_str)
-       #number_for_convert = int (number_for_convert_float) 
+        if not (nombre_a_convertir_str == "") :
+            nombre_a_convertir = float(nombre_a_convertir_str)
+        else :
+            nombre_a_convertir = ""
     except:
         print('ERREUR : Vous devez rentrer un nombre pour convertir')
-        return obtenir_nombre_convertir(number_int)
-    else :
-        if number_int == 1 : #donc pouces vers cm
-            resultat = number_for_convert * 2.54
-            afficher_resultat = str(resultat) + ' cm'
-            return afficher_resultat
-        elif number_int == 2 : #cm vers pouces
-            resultat = number_for_convert * 0.394
-            afficher_resultat = str(resultat) + ' pouces'
-            return afficher_resultat
+        return obtenir_nombre_a_convertir(type_conversion , unite1 , unite2)
 
+    return nombre_a_convertir
 
-def convertir() :
-    while True :
-        number_for_convert_str = obtenir_nombre_convertir(number_int) #obtenir le nombre à convertir
-        if number_for_convert_str == "":
-            return
-        resultat = conversion(number_for_convert_str , number_int) #retourner le resultat obtenu convertion
-        print(resultat)
+def conversion (type_conversion :int, nombre_a_convertir, CONSTANTE : int , unite1 : str ,unite2 :str) :
+    if type_conversion == 1 : #donc pouces vers cm
+        resultat = round(nombre_a_convertir * CONSTANTE , 2)
+        print(resultat , unite2)
+    else : #cm vers pouces
+        resultat = round(nombre_a_convertir / CONSTANTE , 2)
+        print(resultat , unite1)
+        
+######################################################################################################################
 
-
-print("Bienvenue dans le programme de Convertion pouces vers cm ou \n cm vers pouces")
+print(f"Bienvenue dans le programme de Convertion {unite1} vers {unite2} ou \n {unite2} vers {unite1}")
 print("Que Souhaitez vous convertir ? \n'Entrez 1 ou 2 selon la conversion de votre choix'")
-print()
-number_int = demander_type_conversion_User() # pouces en cm ou cm en pouces
-print()
-
-convertir()
-
+type_conversion = demander_type_conversion(unite1 , unite2) # pouces en cm ou cm en pouces
+print("APPUYEZ ENTRER VIDE POUR QUITTER LE PROGRAMME")
+while True :
+    nombre_a_convertir= obtenir_nombre_a_convertir(type_conversion , unite1 , unite2) #obtenir le nombre à convertir
+    if nombre_a_convertir == "":
+        print("fin du programme")
+        break
+    conversion(type_conversion , nombre_a_convertir , CONSTANTE , unite1 ,unite2) #retourner le resultat obtenu convertion
